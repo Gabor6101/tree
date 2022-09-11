aaf4b669bcb5c457731c764d115dc5b5251594fb
Gabor6101 <gerencser.gg.gabor@gmail.com>
2022-09-11 06:13:39 +0200

    i2

--- lib/tree.ex
+++ lib/tree.ex
@@ -3,16 +3,27 @@ defmodule Tree do
   Documentation for `Tree`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Tree.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @spec new(root_name :: String.t(), root_spec :: any(), root_child :: map()) :: map()
+  def new(root_name \\ "root", root_spec \\ nil, root_child \\ nil) do
+    %{
+      root_name => {root_spec, root_child}
+    }
   end
+
+  def add(tree, parent_path, node_name, node_spec \\ nil, node_child \\nil ) do
+    path_list = String.split(parent_path,".",trim: true)
+    chk_path(tree,path_list)
+  end
+
+  defp chk_path(tree,[]) do
+    true
+  end
+  defp chk_path(tree, [H | T]) when tree !== nil and is_map_key(tree,H) do
+    {_, child} = Map.get(tree,H)
+    chk_path(child,T)
+  end
+  defp chk_path(_tree, _path) do
+   false
+  end
+
 end
--- test/tree_test.exs
+++ test/tree_test.exs
@@ -2,7 +2,16 @@ defmodule TreeTest do
   use ExUnit.Case
   doctest Tree
 
-  test "greets the world" do
-    assert Tree.hello() == :world
+  test "create new tree with default root" do
+    assert Tree.new() == %{"root" => {nil,nil}}
+  end
+  test "create new tree with spec root" do
+    assert Tree.new("my root") == %{"my root" => {nil,nil}}
+  end
+  test "create new tree with spec root and value" do
+    assert Tree.new("my root",{1,2,3}) == %{"my root" => {{1,2,3}, nil}}
+  end
+  test "create new tree with spec root and value and child" do
+    assert Tree.new("my root",{1,2,3}, %{}) == %{"my root" => {{1,2,3}, %{}}}
   end
 end
